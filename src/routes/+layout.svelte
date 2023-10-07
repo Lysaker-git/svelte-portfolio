de0de00b842fa3298a73fcaeeaf02abb02dcfa8b
Lysaker-git <68820814+Lysaker-git@users.noreply.github.com>
2023-10-07 17:48:28 +0200

    added font-awesome icon set and set up AppShell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <script>
+	import { AppShell, AppBar, LightSwitch, initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
+
+	
+	initializeStores();
+	const drawerStore = getDrawerStore();
+	
+
 </script>
 
-<slot />
+<Drawer />
+
+<AppShell>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<svelte:fragment slot="footer">
+		<AppBar>
+			<LightSwitch />
+		</AppBar>
+	</svelte:fragment>
+</AppShell>
